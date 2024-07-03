4475c1b3df3564a598efe56e4461e2dee9d2e8be
CJ3022099 <CJ3022099@LAB03-PC15>
2024-07-03 07:46:03 -0300

    aula dia 26

---
+++ Player.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vetores
+{
+    internal class Player
+    {
+        //Atributos
+        private string _name;
+        private int _score;
+
+        //Métodos
+        public Player(string name)
+        {
+            Name = name;
+            _score = 0;
+        }
+        //propriedade
+        public string Name
+        {
+            set
+            {
+                if (!String.IsNullOrEmpty(value))
+                    _name = value;
+            }
+
+            get { return _name; }
+        }
+        public int Score
+        {
+            set
+            {
+                if (value > 0)
+                    _score = value;
+            }
+
+            get { return _score; }
+        }
+        public override string ToString()
+        {
+            return "Nome: " + Name +
+                "\nPontuação: " + Score;
+        }
+    }
+}
